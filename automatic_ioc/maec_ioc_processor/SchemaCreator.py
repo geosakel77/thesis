2574c2a0cb2c5a40154990c6abe00113bf38c923
geosakel77 <mai1428@uom.edu.gr>
2014-12-02 17:34:58 +0200

    Starting day  of the project

---
+++ automatic_ioc/maec_ioc_processor/SchemaCreator.py
@@ -0,0 +1,17 @@
+'''
+Created on 2 Δεκ 2014
+
+@author: george
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
