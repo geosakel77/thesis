fa4bde7bb1a6640bcaaa20aebeff7378e7ec3bff
geosakel77 <mai1428@uom.edu.gr>
2015-07-16 20:48:04 +0300

    maecVocabs dictionary creation

---
+++ automatic_ioc/maec_ioc_processor/MAEC_Bundle.py
@@ -0,0 +1,12 @@
+'''
+Created on 16 Ιουλ 2015
+
+@author: george
+'''
+
+maecVocabs={'ActionObjectAssociationTypeVocab-1.0':['input','output','side-effect'],'DebuggingActionNameVocab-1.0':['check for remote debugger','check for kernel debugger'],
+            'DeviceDriverActionNameVocab-1.1':['load and call driver','load driver','unload driver','emulate driver'],'DirectoryActionNameVocab-1.1':['create directory','delete directory','monitor directory','hide directory'],
+            'DiskActionNameVocab-1.1':['get disk type','get disk attributes','mount disk','unmount disk','emulate disk','list disks','monitor disks'],'DNSActionNameVocab-1.0':['send dns query','send reverse dns lookup'],
+            'FileActionNameVocab-1.1':['create file','delete file','copy file','create file symbolic link','find file','get file attributes','set file attributes','lock file','unlock file','modify file','move file','open file','read from file','write to file','rename file','create file alternate data stream','send control code to file','create file mapping','open file mapping','execute file','hide file','close file'],
+            }
+maecBundle={'maecVocabs':maecVocabs} 
